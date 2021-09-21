c09cf1aa166d1894d227024324b0f5df7f4fc588
Raj <rajdipshah01!gmail.com>
2021-09-21 19:54:16 +0545

    SHEESH more CODES

--- CSS inventory/home.js
+++ CSS inventory/home.js
@@ -1,5 +1,8 @@
+// <!-- -----------------------------Togglle Menu --------------------------->
+
 const menuBtn = document.querySelector('.menu-btn');
 
 function menuToggle(){
     menuBtn.classList.toggle('open')
 }
+// <!-- -----------------------------Togglle Menu --------------------------->
